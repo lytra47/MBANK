200170c5e858ee7c897104ac4de783b75fd7df90
lytra47 <lytra47@proton.me>
2022-07-01 20:57:59 +0530

    Initial mobile layout

---
+++ script.js
@@ -0,0 +1,11 @@
+const modal = document.querySelector(".modal1");
+const overlay = document.querySelector(".overlay");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
