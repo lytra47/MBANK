43d6d9257c30433f086d1ac2553486a24824140f
lytra47 <lytra47@proton.me>
2022-07-02 22:42:42 +0530

    design and color changed, added responsiveness, added payment modal

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const modal = document.querySelector(".modal1");
 const overlay = document.querySelector(".overlay");
+const pay = document.querySelector(".payments");
 
 const openModal = function () {
   modal.classList.remove("hidden");
@@ -8,5 +9,12 @@ const openModal = function () {
 const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
+  pay.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 overlay.addEventListener("click", closeModal);
+
+const paymentsO = function () {
+  pay.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
