6e3d6bc159c71a6839584903853d995d998c71af
lytra47 <lytra47@proton.me>
2022-07-03 11:06:16 +0530

    ui color adjustments and navbar responsiveness adjustments

--- script.js
+++ script.js
@@ -18,3 +18,43 @@ const paymentsO = function () {
   pay.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
+
+const addUSR = function () {
+  acUSR = {
+    id: uid.value,
+    name: uname.value,
+    acno: acno.value,
+    pswd: pword.value,
+  };
+  if (acUSR.id in localStorage) {
+    alert(`Account already exist, please login.`);
+  } else {
+    localStorage.setItem(acUSR.id, JSON.stringify(acUSR));
+    alert("Account creation successfully.");
+    closeModal();
+  }
+};
+
+const login = function () {
+  if (usrID.value in localStorage) {
+    if (
+      usrPSWD.value == JSON.parse(localStorage.getItem(usrID.value))["pswd"]
+    ) {
+      window.location = "home.html";
+      localStorage.setItem("username", usrID.value);
+    } else {
+      alert("Incorrect password;");
+    }
+  } else {
+    alert("User not found. Please register");
+  }
+};
+
+const logout = function () {
+  // localStorage.removeItem(user);
+  window.location = "index.html";
+};
+
+document.getElementById("welc").innerHTML = `Welcome, ${localStorage.getItem(
+  "username"
+)}`;
